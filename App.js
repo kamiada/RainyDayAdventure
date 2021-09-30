24bc5c551d12755fdc0f43a010da4d1254e6f0d4
kamina04 <adrianna.kaminska@bbc.co.uk>
2021-09-30 12:17:29 +0100

    more stuff

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Menu } from "../src/pages";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body>
+        <Menu />
+      </body>
+    </div>
+  );
+}
+
+export default App;
