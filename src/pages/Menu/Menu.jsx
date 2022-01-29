12ed9e29e76dfabaac5fe182d2a142bde9a68d6a
kamiada <kamiada528@gmail.com>
2022-01-29 17:16:22 +0000

    sorting this out

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -6,13 +6,13 @@ import "../pages.scss";
 const Menu = () => {
   return (
     <div>
-      <h1>The Duality of Choice</h1>
       <Router>
-        {/* <Route path="/scene1" element={<Scene_1 />} />
-          <Link to="/scene1">Start Your Adventure!</Link> */}
-        {/* react looks at routes and matches with the first one which it finds so / instead of the right /something  - exact prop helps with that*/}
         <Switch>
-          <Route exact path="/scene">
+          <Route exact path="/">
+          {/* Menu page */}
+          <h1>The Duality of Choice</h1>
+          </Route>
+          <Route exact path="/scene"> Here
             <Scene_1 />
           </Route>
         </Switch>
