04cec119635f7cd5a8726a0b1fa7c8c87afd771d
kamiada <kamiada528@gmail.com>
2022-01-29 20:05:16 +0000

    intro screen set up

--- src/pages/Scene_1/Scene_1.jsx
+++ src/pages/Scene_1/Scene_1.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import '../pages.scss';
+import React, { useState } from "react";
+import "../pages.scss";
 import { TextHolder } from "../../components";
+import introduction from "../../staticText.json";
+import responses from "../../ChoiceTextAdventure.json";
 
 const Scene_1 = () => {
-    return (
-    <div>
-        Bo1
+  const [id, setIdNumb] = useState(0);
+
+  return (
+    <div className="background">
+      <div>
+        <TextHolder intro_tex={introduction.staticText_introduction[0].text} />
+        <button onClick={() => setIdNumb(id + 1)}>Next</button>
+      </div>
     </div>
-    )
+  );
 };
 
 export default Scene_1;
--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,18 @@ module.exports = {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       {test : /\.(jsx)$/, use:'babel-loader'},
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
     ]
   },
   devServer: {
