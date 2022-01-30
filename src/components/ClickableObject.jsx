6953166303dcbb56b5abb1a7d492a805e80a2f52
kamiada <kamiada528@gmail.com>
2022-01-30 13:34:32 +0000

    hour and a half till the end

--- src/components/ClickableObject.jsx
+++ src/components/ClickableObject.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import "./components.scss";
 
-export const ClickableObject = (props)  => {
-    return (
-        <div>
-            <button id={props.id } className="object" onClick={props.onClick}>
-            <img  src={props.thisIs} />
-            </button>
-        </div>
-    )
-}
+export const ClickableObject = (props) => {
+  return (
+    <button id={props.id} className="object" onClick={props.onClick}>
+      <img src={props.thisIs} />
+    </button>
+  );
+};
 
 export default ClickableObject;
--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../pages.scss";
 import { ClickableObject } from "../../components";
 import { Table } from "../images";
@@ -9,6 +9,7 @@ import {
   PokeweedBerries,
   Dandelion,
 } from "../../images_objects";
+import { Link } from "react-router-dom";
 
 // Right combo:
 // 1. Dandelion 2. Vervain 3. Powder
@@ -16,20 +17,48 @@ import {
 //1. Dandelion
 //2. Pokeweed Berries
 //3. Green Powder
-
-const chosenObject = () => {
-  return console.log("clicked");
-};
-
+const ingridientList = [];
 const Game = () => {
+  const chosenObject = (ingridient) => {
+    ingridientList.push(ingridient);
+    console.log(ingridientList);
+
+    return console.log("clicked");
+  };
+
   return (
     <>
-      <img className="game_table" src={Table} />
-      <ClickableObject id="Dandelion" onClick={() => chosenObject()} thisIs={Dandelion} />
-      <ClickableObject id="Vervain" onClick={() => chosenObject()} thisIs={Vervain}/>
-      <ClickableObject id="PokeweedBerries" onClick={() => chosenObject()} thisIs={PokeweedBerries}/>
-      <ClickableObject id="GreenPowder" onClick={() => chosenObject()} thisIs={GreenPowder}/>
-
+      <>
+        <img className="game_table" src={Table} />
+        <img className="flask" src={Flask} />
+        <div className="object_holder">
+        <ClickableObject
+          id="Dandelion"
+          onClick={() => chosenObject("dandelion")}
+          thisIs={Dandelion}
+        />
+        <ClickableObject
+          id="Vervain"
+          onClick={() => chosenObject("vervain")}
+          thisIs={Vervain}
+        />
+        <ClickableObject
+          id="PokeweedBerries"
+          onClick={() => chosenObject("berries")}
+          thisIs={PokeweedBerries}
+        />
+        <ClickableObject
+          id="GreenPowder"
+          onClick={() => chosenObject("powder")}
+          thisIs={GreenPowder}
+        />
+        </div>
+      </>
+      <div className="link_holder">
+      <Link className="finished" to="/final">
+        Done!
+      </Link>
+      </div>
     </>
   );
 };
