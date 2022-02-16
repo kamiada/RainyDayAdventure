eb99f961daf512a6777cda4f88a8b75dba44728d
kamiada <kamiada528@gmail.com>
2022-02-16 14:17:42 +0000

    playing around

---
+++ Backend/README.md
@@ -0,0 +1,4 @@
+# This readme is more for me for testing and developing (and not forgetting things) than anything else
+
+In order to run server on localhost: 
+1. In console `cd Backend` and then `node src/server.js`
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,2 +1,19 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
+const typeDefs = `
+    type Query {
+        info: String!
+    }
+`;
+const resolvers = {
+  Query: {
+    info: () => `Testing if it works`,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
